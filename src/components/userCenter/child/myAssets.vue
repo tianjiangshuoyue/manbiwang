15507d61c4ec98222ef46b65441d47e0b332b683
wuxiaolan <wuxiaolan91@163.com>
2017-08-06 15:26:02 +0800

    修改我的资产图表

--- src/components/userCenter/child/myAssets.vue
+++ src/components/userCenter/child/myAssets.vue
@@ -1,41 +1,49 @@
 <template>
-     <div>
+    <div>
         <!-- 为 ECharts 准备一个具备大小（宽高）的 DOM -->
         <div id="main" style="width: 600px;height:400px;"></div>
         <section>
-            总资产:<em class="money">16000</em>
+            总资产:
+            <em class="money">16000</em>
         </section>
         <ul>
             <li></li>
-            </ul>
+        </ul>
     </div>
 </template>
 <script>
 export default {
-    data () {
+    data() {
         return {
-            data:{
-                
+            data: {
+
             }
         }
-    }, mounted () {// 基于准备好的dom，初始化echarts实例
+    }, mounted() {// 基于准备好的dom，初始化echarts实例
         var myChart = echarts.init(document.getElementById('main'));
         let option = {
             tooltip: {
                 trigger: 'item',
-                formatter: "{b}: {c} ({d}%)"
+                formatter: "{b} <br/> {c} ({d}%)"
             },
             legend: false,
             series: [
                 {
-                    type:'pie',
+                    type: 'pie',
                     radius: ['50%', '70%'],
                     center: ['50%', '60%'],
-                   itemStyle: {
+                    avoidLabelOverlap: false,
+                    label: {
+                        normal: {
+                            show: false,
+                            position: 'center'
+                        },
                         emphasis: {
-                            shadowBlur: 10,
-                            shadowOffsetX: 0,
-                            shadowColor: 'rgba(0, 0, 0, 0.5)'
+                            show: true,
+                            textStyle: {
+                                fontSize: '30',
+                                fontWeight: 'bold'
+                            }
                         }
                     },
                     labelLine: {
@@ -43,46 +51,46 @@ export default {
                             show: false
                         }
                     },
-                    data:[
+                    data: [
                         {
-                            value:335,
-                            name:'人民币',
+                            value: 335,
+                            name: '人民币',
                             itemStyle: {
                                 normal: {
                                     color: '#fa84b4'
                                 }
                             }
                         },
-                         {
-                            value:335,
-                            name:'以太坊',
+                        {
+                            value: 335,
+                            name: '以太坊',
                             itemStyle: {
                                 normal: {
                                     color: '#7a9a80'
                                 }
                             }
                         },
-                         {
-                            value:335,
-                            name:'比特币',
+                        {
+                            value: 335,
+                            name: '比特币',
                             itemStyle: {
                                 normal: {
                                     color: '#f7921a'
                                 }
                             }
                         },
-                         {
-                            value:335,
-                            name:'小蚁股',
+                        {
+                            value: 335,
+                            name: '小蚁股',
                             itemStyle: {
                                 normal: {
                                     color: '#2b98f0'
                                 }
                             }
                         },
-                         {
-                            value:335,
-                            name:'莱特币',
+                        {
+                            value: 335,
+                            name: '莱特币',
                             itemStyle: {
                                 normal: {
                                     color: '#bebebe'
